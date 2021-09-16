0cdb0ba2a3e25f9d7138e665bbd99fa7084d4cea
minchan481 <55022230+minchan481@users.noreply.github.com>
2021-09-16 13:43:41 +0900

    Create 04_example_3.ino

---
+++ 04_example_3.ino
@@ -0,0 +1,24 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+// LED ON/OUT within 1sec.
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while(!Serial){
+    ; //wait for serial port to connect
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle); // turn off LED
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle); //toggle LED value
+  digitalWrite(PIN_LED, toggle); // update LED status
+  delay(1000); // wait for 1sec.
+}
+
+int toggle_state(int toggle){
+  return !toggle;
+}
