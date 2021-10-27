28e936e266c23e809a238ab5369c7bf7248b0524
minchan481 <55022230+minchan481@users.noreply.github.com>
2021-10-28 05:24:36 +0900

    Add files via upload

---
+++ 20212972-Lab07-3/src/References1.java
@@ -0,0 +1,41 @@
+// *********************************************************************
+// References1.java
+//
+// Illustrates aliases and references
+// *********************************************************************
+public class References1
+{
+	public static void main(String[] args)
+	{
+		Person person1 = new Person("Rachel", 6);
+		Person person2 = new Person("Elly", 4);
+		Person person3 = new Person("Sarah", 19);
+		Person person4 = new Person("", 1);
+		
+		System.out.println("\nThe three original people...");
+		System.out.println(person1 + ", " + person2 + ", " + person3);
+		
+		// Reassign people
+		person4 = person3;
+		person3 = person2;
+		person2 = person1;
+		person1 = person4;
+		
+		System.out.println("\nThe three people reassigned...");
+		System.out.println(person1 + ", " + person2 + ", " + person3);
+		
+		System.out.println();
+		System.out.println("Changing the second name to Bozo...");
+		person2.changeName("Bozo");
+		System.out.println(person1 + ", " + person2 + ", " + person3);
+		
+		System.out.println();
+		System.out.println("Changing the third name to Clarabelle...");
+		person3.changeName("Clarabelle");
+		System.out.println(person1 + ", " + person2 + ", " + person3);
+		System.out.println();
+		System.out.println("Changing the first name to Harpo...");
+		person1.changeName("Harpo");
+		System.out.println(person1 + ", " + person2 + ", " + person3);
+	}
+}
